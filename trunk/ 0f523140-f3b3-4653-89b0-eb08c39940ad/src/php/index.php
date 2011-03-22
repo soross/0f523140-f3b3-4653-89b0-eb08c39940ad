cf37ed11ffbda77b5f1feb0c1a1d2464a17a9a8e
felixcat.gng@gmail.com <felixcat.gng@gmail.com@71d3263d-18f7-1632-4e8d-853502444960>
2011-03-22 02:34:27 +0000

    Follow&History callbacks added & big fix for login

--- trunk/ 0f523140-f3b3-4653-89b0-eb08c39940ad/src/php/index.php
+++ trunk/ 0f523140-f3b3-4653-89b0-eb08c39940ad/src/php/index.php
@@ -4,6 +4,11 @@ session_start();
 include_once('common.inc.php');
 include_once('theme.php');
 include_once('login.php');
+include_once('search.php');
+include_once('follow.php');
+include_once('favorite.php');
+#include_once('.php');
+#include_once('.php');
 
 func_register(array(
   '' => array(
@@ -11,17 +16,23 @@ func_register(array(
   ),
 ));
 
+function theme_home($content)
+{
+    theme('page', "首页", $content);
+}
+
 function home_page()
 {
     $content = "Hello world!<br>";
     if(user_is_authenticated())
     {
-        $content .= $GLOBALS['user']['nickname']."已登陆";
+        $content .= $GLOBALS['user']['nickname']."已登陆<br/><a href='logout'>退出</a>";
     }
     else
         $content .= "尚未登陆, 请点击<a href='login'>这里</a>登陆.";
-    theme('page', "首页", $content);
+    theme_home($content);
 }
 
 func_execute_active_handler();
 ?>
+
