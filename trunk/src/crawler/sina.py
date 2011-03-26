c418913f604eddccd399d004b92f162907ac3f9f
felixcat.gng@gmail.com <felixcat.gng@gmail.com@71d3263d-18f7-1632-4e8d-853502444960>
2011-03-26 04:20:52 +0000

    crawler: Done initial craw, now reverting to 2 pages per craw. API rate: 2 / hour (so low!)

--- trunk/src/crawler/sina.py
+++ trunk/src/crawler/sina.py
@@ -8,7 +8,7 @@ from Queue import Queue
 from time import sleep
 from datetime import datetime
 
-PAGE = 10
+PAGE = 2
 A = [(u"新媒体", u"小王trueman", "1f8f7db82cdbc346a91840cef2bc1cb9", "a16ead9ee3b6b3f43e601de127275ddc"),
 	 (u"风投/投行", u"小毕Simon", "4151efe34301f5fddb9d34fc72e5f4a4", "dc4a07e8b7936d688726604a7442e4bc"),
 	 (u"移动互联", u"冬冬Billy", "f6449dd703d66cf2be5274f321416958", "31c47db4cd79e43a9196871a554d0847"),
