848d48a01b2fda9e1b318bcadbb3b933437b59c0
felixcat.gng <felixcat.gng@gmail.com>
2011-03-28 01:18:39 +0000

    changed default page.

---
+++ src/html/config.inc.php
@@ -0,0 +1,19 @@
+<?php
+
+$db_host = '127.0.0.1';
+$db_name = 'apis';
+$db_user = 'apis';
+$db_pass = 'apis';
+
+define("SINA_AKEY", '961495784');
+define("SINA_SKEY", '47d9d806a1dc04cc758be6f7213465bc');
+
+define("ENCRYPTION_KEY", 'HustGNGisVeryGelivable');
+
+$base_url = 'http://'.$_SERVER['HTTP_HOST'];
+if ($directory = trim(dirname($_SERVER['SCRIPT_NAME']), '/\,')) {
+  $base_url .= '/'.$directory;
+}
+define('BASE_URL', $base_url.'/');
+
+?>
---
+++ src/html/search.inc.php
@@ -0,0 +1,217 @@
+<?php
+func_register(array(
+    'search' => array(
+        'callback' => 'search_page',
+    ),
+    'history' => array(
+        'callback' => 'search_history',
+        'security' => 'true',
+    ),
+    'rss' => array(
+        'callback' => 'get_rss',
+    ),
+));
+
+function theme_search($title, $content)
+{
+    if($title=='首页')
+    {
+        $GLOBALS['search'] = "淘宝";
+        include_once("common.inc.php");
+        $counts = get_counts();
+        $pre = '<div id="radio">
+                本周新增职位'.$counts['tweets_thisweek'].'个，今日新增职位'.$counts['tweets_today'].'个</div>
+                <div id="microblogs">';
+    }
+    else
+    {
+        $key = mb_substr($title, 0, -9);
+        $GLOBALS['search'] = $key;
+        $pre = '<div id="microblogs"><div id="search-result-outer">
+                    <div id="search-result">
+                        <div class="left">
+                            #<a class="keyword">'.$key.'</a>#的搜索结果</div>';
+        include_once("follow.inc.php");
+        if(!following_exist($key))
+            $pre .= '<a id="search-result-concern" class="left" href="'.BASE_URL.'follow/add/'.$key.'"></a>';
+        $pre .= '<a id="search-result-rss" class="right">
+                        </a>
+                    </div>
+                </div>';
+    }
+    $content = $pre.$content.'</div>';
+    theme('page', $title, $content);
+}
+
+function get_search_result($key, $num)
+{
+    connect_db();
+    
+    //$view = "SELECT * FROM tweets WHERE MATCH (content) AGAINST ('$key') ORDER BY post_datetime DESC";
+    //FIXME: Cannot use this syntax.
+    
+    $key = explode(" ",$key);
+    $key = "%".implode("%",$key)."%";
+    
+    $view = "SELECT * FROM tweets WHERE content LIKE '$key' ORDER BY post_datetime DESC LIMIT 0 , $num";
+    //FIXME: Low performance!
+    
+    $list = mysql_query($view);
+    $result = array();
+    $i = 0;
+    while($row = mysql_fetch_array($list))
+    {
+        $result[$i++] = $row;
+        if($i == $num)
+            break;
+    }
+    return $result;
+}
+
+function get_newest_result($num)
+{
+    connect_db();
+    $view = "SELECT * FROM tweets ORDER BY post_datetime DESC LIMIT 0 , $num";
+    
+    $list = mysql_query($view);
+    $result = array();
+    $i = 0;
+    while($row = mysql_fetch_array($list))
+    {
+        $result[$i++] = $row;
+        if($i == $num)
+            break;
+    }
+    return $result;
+}
+
+function search_page($query)
+{
+    $key = (string) $query[1];
+    if(!$key)
+    {
+        $key = $_POST['search_text'];
+        if(!$key)
+            die("Invalid argument!");
+    }
+    include_once('login.inc.php');
+    if(user_is_authenticated())
+        search_history_add("", "", $key);
+    $data = get_search_result($key, 10);
+    $content = theme('result', $data);
+    theme('search', $key." - 搜索", $content);
+}
+
+function get_rss($query)
+{
+    $key = (string) $query[1];
+    if(!$key)
+    {
+        $key = $_POST['search_text'];
+        if(!$key)
+            die("Invalid argument!");
+    }
+    $data = get_search_result($key, 10);
+    $GLOBALS['search'] = $key;
+    theme('rss', $data);
+}
+
+function get_search_history($num)
+{
+    include_once('login.php');
+    $id = get_current_user_id();
+    connect_db();
+    $view = "SELECT * FROM searchhistory WHERE user_id='$id' AND deleted=0 ORDER BY add_time DESC";
+    $list = mysql_query($view);
+    $result = array();
+    $i = 0;
+    while($row = mysql_fetch_array($list))
+    {
+        $result[$i++] = $row;
+        if($i == $num)
+            break;
+    }
+    return $result;
+}
+
+function search_history_show()
+{
+    $args = func_get_args();
+    $key = intval($args[2]);
+    $data = get_search_history($key);
+    $content = "";
+    foreach($data as $s)
+    {
+        $content .= $s['search']."<br />";
+    }
+    theme('page', '搜索历史', $content);
+}
+
+function search_history_delete()
+{
+    include_once('login.php');
+    $id = get_current_user_id();
+    $args = func_get_args();
+    $key = $args[2];
+    if(!$key)
+        die('Invalid Argument!');
+    connect_db();
+    $view = "SELECT * FROM searchhistory WHERE user_id='$id' AND history_id='$key' AND deleted='0'";
+    $list = mysql_query($view);
+    $row = mysql_fetch_array($list);
+    if($row)
+    {
+        $view = "UPDATE searchhistory SET deleted='1' WHERE user_id='$id' AND history_id='$key'";
+        $list = mysql_query($view) or die("Delete error!");
+    }
+    else
+    {
+        print $key;
+        die(": Non-exist Error!");
+    }
+}
+
+function search_history_deleteall()
+{
+    include_once('login.php');
+    $id = get_current_user_id();
+    connect_db();
+    $view = "UPDATE searchhistory SET deleted='1' WHERE user_id='$id'";
+    $list = mysql_query($view) or die("Delete error!");
+    header("Location: ".BASE_URL);
+}
+
+function search_history_add()
+{
+    include_once('login.php');
+    $id = get_current_user_id();
+    $args = func_get_args();
+    $key = $args[2];
+    if(!$key)
+        die('Invalid Argument!');
+    connect_db();
+    $view = "SELECT * FROM searchhistory WHERE user_id='$id' AND search='$key' AND deleted='0'";
+    $list = mysql_query($view);
+    $row = mysql_fetch_array($list);
+    if($row)
+    {
+        search_history_delete("", "", $row['history_id']);
+    }
+    include_once("uuid.inc.php");
+    $v4uuid = str_replace("-", "", UUID::v4());
+    $current_datetime = date('Y-m-d H:i:s');
+    $view = "INSERT INTO searchhistory(history_id, search, user_id, deleted, add_time) VALUES ('$v4uuid', '$key', '$id', '0', '$current_datetime')";
+    $list = mysql_query($view) or die($view."Insert error!");
+}
+
+function search_history($query)
+{
+    $key = (string) $query[1];
+    if(!$key)
+        $key = "show";
+    $function = 'search_history_'.$key;
+    if (!function_exists($function))
+        die("Invalid Argument!");
+    return call_user_func_array($function, $query);
+}
+?>
