babae6dfef3bf306ce8354630f1d8d2558d4f84c
power80766@gmail.com <power80766@gmail.com@71d3263d-18f7-1632-4e8d-853502444960>
2011-03-29 03:00:15 +0000

    change the color of search bar

--- trunk/src/html/script/share.js
+++ trunk/src/html/script/share.js
@@ -108,11 +108,13 @@ function Init() {
     $("input#search-text").focusin(function () {
         if ($(this).val() == "职位关键字，如：北京 产品经理 阿里巴巴") {
             $(this).val("");
+            $(this).css("color", "#000000");
         }
     });
     $("input#search-text").focusout(function () {
         if ($(this).val() == "") {
             $(this).val("职位关键字，如：北京 产品经理 阿里巴巴");
+            $(this).css("color", "#adadad");
         }
     });
 }
