3784070b7ae12f505c662836fc740fba5a18a0d9
power80766 <power80766@gmail.com>
2011-03-29 01:30:26 +0000

    move the login event to share.js

--- src/html/script/manager.js
+++ src/html/script/manager.js
@@ -40,3 +40,24 @@ function ShowNormal() {
     $("span.item-apply").hide();
     $("a.item-favourite").hide();
 }
+
+function AfterLogin() {
+    $.ajax({
+        type: 'POST',
+        url: 'info/',
+        success: function (msg) {
+            $(".logined").show();
+            $(".logouted").hide();
+            $("a#name").html(msg.split(',')[0]);
+            var type = msg.split(',')[1];
+            if (type == 1) {
+                $(".jobs").show();
+                $(".recruitment").hide();
+            }
+            else {
+                $(".jobs").hide();
+                $(".recruitment").show();
+            }
+        }
+    });
+}
