43414e1bcfd0fe170df73be0828464d540342b61
power80766@gmail.com <power80766@gmail.com@71d3263d-18f7-1632-4e8d-853502444960>
2011-04-02 13:05:48 +0000

    Add the help and feedback page

---
+++ trunk/src/html/script/help_feedback.js
@@ -0,0 +1,20 @@
+﻿function AfterLogin() {
+    $.ajax({
+        type: 'POST',
+        url: 'info/',
+        success: function (msg) {
+            $(".logined").show();
+            $(".logouted").hide();
+            $("a#name").html(msg.split(',')[0]);
+            var type = msg.split(',')[1];
+            if (type == 1) {
+                $(".jobs").show();
+                $(".recruitment").hide();
+            }
+            else {
+                $(".jobs").hide();
+                $(".recruitment").show();
+            }
+        }
+    });
+}
