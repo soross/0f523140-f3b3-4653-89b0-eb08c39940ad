54417597e3ba0273d85dd19ef2c241bf8b783631
felixcat.gng@gmail.com <felixcat.gng@gmail.com@71d3263d-18f7-1632-4e8d-853502444960>
2011-04-03 12:36:21 +0000

    hot tag ported

--- trunk/src/html/tag.inc.php
+++ trunk/src/html/tag.inc.php
@@ -1,4 +1,9 @@
 <?php
+func_register(array(
+    'hot' => array(
+        'callback' => 'theme_hot',
+    ),
+));
 
 function get_tags($id)
 {
@@ -11,11 +16,50 @@ function get_tags($id)
     while($row = mysql_fetch_array($list))
     {
         $result[$i++] = $row[0];
-        if($i == $num)
+        if($i == 7)
             break;
     }
     if($i == 0)
         return false;
     return $result;
+}
+
+function get_hot($num)
+{
+    include_once("common.inc.php");
+    connect_db();
+    $view = "SELECT name,count,tag_group from tags ORDER BY count DESC";
+    $list = mysql_query($view);
+    $result = array();
+    $i = 0;
+    $g = array();
+    $j = 0;
+    while($row = mysql_fetch_array($list))
+    {
+        $ok = true;
+        foreach($g as $group)
+            if($g == $row['tag_group'])
+            {
+                $ok = false;
+                break;
+            }
+        if(!$ok)
+            continue;
+        $result[$i++] = $row[0];
+        if($i == $num)
+            break;
+        if($row['tag_group']!=0)
+            $g[$j++] ++;
+    }
+    return $result;
+}
+
+function theme_hot($num)
+{
+    $content = "";
+    $hots = get_hot(12);
+    foreach($hots as $h)
+        $content .= '<a class="left hot-content-item">'.$h['name'].'('.$h['count'].')</a>';
+    echo $content;
 }
 ?>
