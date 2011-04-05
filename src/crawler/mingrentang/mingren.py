c06746f2da2eda18bc03d2d707fcea54e33096b1
felixcat.gng <felixcat.gng@gmail.com>
2011-04-05 18:20:42 +0000

    mingrentang crawler and all results.

--- src/crawler/mingrentang/mingren.py
+++ src/crawler/mingrentang/mingren.py
@@ -1,30 +1,37 @@
+#coding:utf-8
 #!/usr/bin/env python
 from threading import Thread
 from Queue import Queue
 from time import sleep
 import sqlite3
-import sys, re, StringIO
+import sys, re, StringIO, os
 import urllib2 as urllib
 q = Queue()
 NUM = 17
 
-def craw(key):
+def craw(key, tag, tag2):
     a = unicode(urllib.urlopen(key).read(), "utf-8")
+    f = open(tag + os.sep + tag2 + ".txt", "w")
+    i = 0
     while True:
-        print re.findall("<a href=\"home.php?uid=(\d+)&amp;[^\"]+\">([^<]+)</a>", a)
-        raw_input()
+        i += 1
+        print "Crawler",tag,tag2,"Page",i,"Crawing..."
+        b = re.findall("<a href=\"home.php\?uid=(\d+)&amp;[^\"]+\">([^<]+)</a>", a)
+        for userid, nickname in b:
+            f.write((userid + '\t' + nickname + '\n').encode('utf-8'))
         try:
-            nextpage = re.findall("<a href=\"([^\"]+)\">下页</a>", b[-3])[0].replace("&amp;", "&")
+            nextpage = re.findall(u"<a href=\"([^\"]+)\">下页</a>", a)[0].replace("&amp;", "&")
         except:
+            f.close()
             return
         sleep(0.1)
-        a = urllib.urlopen("http://t.sina.cn/dpool/ttt/" + nextpage)
+        a = unicode(urllib.urlopen("http://t.sina.cn" + nextpage).read(), "utf-8")
 
 def working():
     global q
     while True:
         arguments = q.get()
-        craw(arguments)
+        craw(*arguments)
         q.task_done()
 
 for i in range(NUM):
@@ -37,8 +44,13 @@ a = unicode(urllib.urlopen(url).read(), "utf-8")
 b = re.findall("<a href=\"(/dpool/ttt/v2star.php\?cat=1&amp;ta[^\"]+)\">([^<]+)</a>", a)
 for (url, tag) in b:
     print tag
-    url = 'http://t.sina.cn/' + url.replace('&amp;', '&')
+    try:
+        os.mkdir(tag)
+    except:
+        pass
+    url = 'http://t.sina.cn' + url.replace('&amp;', '&')
     a = unicode(urllib.urlopen(url).read(), "utf-8")
     b2 = re.findall("<a href=\"(/dpool/ttt/v2star.php\?cat=1&amp;su[^\"]+)\">([^<]+)</a>", a)
-    for (url, tag) in b2:
-        print url, tag
+    for (url, tag2) in b2:
+        q.put(('http://t.sina.cn' + url.replace('&amp;', '&'), tag, tag2))
+q.join()
