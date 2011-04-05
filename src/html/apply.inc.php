24347c38b7dabe1199f6418d9f47c4b271587ac5
felixcat.gng <felixcat.gng@gmail.com>
2011-04-05 01:47:01 +0000

    added a lot of things

---
+++ src/html/apply.inc.php
@@ -0,0 +1,154 @@
+<?php
+func_register(array(
+    'apply' => array(
+        'callback' => 'user_apply',
+        'security' => 'true',
+    ),
+));
+
+function apply_count()
+{
+    include_once('login.inc.php');
+    $id = get_current_user_id();
+    connect_db();
+    $view = "SELECT COUNT(*) from FROM applications WHERE user_id='$id' AND deleted = 0";
+    $list = mysql_query($view);
+    $row = mysql_fetch_array($list);
+    echo $row['COUNT(*)'];
+}
+
+function get_applies($num, $page)
+{
+    include_once('login.inc.php');
+    $id = get_current_user_id();
+    if(!$page)
+        $page = "0";
+    $page = intval($page) * $num;
+    $limit = " LIMIT $page , $num";
+    connect_db();
+    $view = "SELECT tweets.* from tweets, (SELECT * FROM applications WHERE user_id='$id' AND deleted=0) as applications WHERE tweets.tweet_id=applications.tweet_id ORDER BY tweets.post_datetime DESC$limit";
+    $list = mysql_query($view);
+    $result = array();
+    $i = 0;
+    while($row = mysql_fetch_array($list))
+        $result[$i++] = $row;
+    return $result;
+}
+
+function apply_show()
+{
+    $args = func_get_args();
+    $key = intval($args[2]);
+    if($args[3])
+        $page = intval($args[3]);
+    else
+        $page = "";
+    $content = '';
+    $favorites = get_applies($key, $page);
+    foreach($favorites as $f)
+    {
+        $content .= '<div class="item" id="'.$f['tweet_id'].'">
+                   <div class="item-delete">
+                       <a class="right"></a>
+                   </div>
+                   <div class="left item-pic">
+                       <img src="'.$f['profile_image_url'].'" alt="" width="50" height="50" />
+                   </div>
+                   <div class="left item-content">
+                       <div class="item-blog">
+                           <a class="item-blog-name">'.$f['post_screenname'].'</a>：'.$f['content'].'
+                       </div>
+                       <div class="item-other">
+                           <span class="left item-time">'.$r['post_datetime'].'</span> '.$source.'
+                           <a class="right item-favourite item-action delete">取消收藏</a> ';
+                            if($f['type'] != 1)
+                                $content .= '<a class="right item-favourite item-doapply apply">
+                                        申请该职位</a>';
+        $content .= '
+                       </div>
+                   </div>
+                   <div class="clear">
+                   </div>
+               </div>';
+    }
+    echo $content;
+}
+
+function apply_delete()
+{
+    include_once('login.php');
+    $id = get_current_user_id();
+    $args = func_get_args();
+    $key = $args[2];
+    if(!$key)
+        die('Invalid Argument!');
+    connect_db();
+    $view = "SELECT * FROM applications WHERE user_id='$id' AND tweet_id='$key' AND deleted='0'";
+    $list = mysql_query($view);
+    $row = mysql_fetch_array($list);
+    if($row)
+    {
+        $view = "UPDATE applications SET deleted='1' WHERE user_id='$id' AND tweet_id='$key'";
+        $list = mysql_query($view) or die("Delete error!");
+    }
+    else
+    {
+        print $key;
+        die(": Non-exist Error!");
+    }
+}
+
+function apply_exist()
+{
+    include_once('login.php');
+    $id = get_current_user_id();
+    $args = func_get_args();
+    $key = $args[2];
+    if(!$key)
+        die('Invalid Argument!');
+    connect_db();
+    $view = "SELECT * FROM applications WHERE user_id='$id' AND tweet_id='$key' AND deleted='0'";
+    $list = mysql_query($view);
+    $row = mysql_fetch_array($list);
+    if($row)
+        echo "1";
+    else
+        echo "0";
+}
+
+function apply_add()
+{
+    include_once('login.php');
+    $id = get_current_user_id();
+    $args = func_get_args();
+    $key = $args[2];
+    if(!$key)
+        die('Invalid Argument!');
+    connect_db();
+    $view = "SELECT * FROM applications WHERE user_id='$id' AND tweet_id='$key' AND deleted='0'";
+    $list = mysql_query($view);
+    $row = mysql_fetch_array($list);
+    if($row)
+    {
+        applies_delete("", "", $key);
+    }
+    $view = "INSERT INTO applications(resume_id, tweet_id, user_id) VALUES ('$id', '$key', '$id')";
+    $list = mysql_query($view) or die("Insert error!");
+}
+
+function theme_apply($content)
+{
+    theme('page', "申请", $content);
+}
+
+function user_apply($query)
+{
+    $key = (string) $query[1];
+    if(!$key)
+        $key = "show";
+    $function = 'apply_'.$key;
+    if (!function_exists($function))
+        die("Invalid Argument!");
+    return call_user_func_array($function, $query);
+}
+?>
---
+++ src/html/resume.inc.php
@@ -0,0 +1,88 @@
+<?php
+func_register(array(
+    'resume' => array(
+        'callback' => 'deal_resume',
+        'security' => 'true',
+    ),
+));
+
+function resume_add()
+{
+    include_once('login.php');
+    include_once("uuid.inc.php");
+    #$resume_id = str_replace("-", "", UUID::v4());
+    
+    $user_id = get_current_user_id();
+    $resume_id = $user_id;
+    $name = $_POST['name'];
+    $sex = $_POST['sex'];
+    $date_birth = $_POST['date_birth'];
+    $live_in_now = $_POST['live_in_now'];
+    $live_in = $_POST['live_in'];
+    $cellphone = $_POST['cellphone'];
+    $email = $_POST['email'];
+    $content = $_POST['content'];
+    
+    connect_db();
+    $view = "SELECT * FROM resumes WHERE resume_id = '$resume_id'";
+    $list = mysql_query($view);
+    $row = mysql_fetch_array($list);
+    if($row)
+    {
+        $view = "UPDATE resume SET name='$name', sex='$sex', date_birth='$date_birth', live_in_now='$live_in_now', live_in='$live_in', cellphone='$cellphone', email='$email', content='$content' WHERE resume_id = '$resume_id'";
+        $list = mysql_query($view) or die("Update error!");
+    }
+    else
+    {
+        $view = "INSERT INTO resume(user_id, resume_id, name, sex, date_birth, live_in_now, live_in, cellphone, email, content) VALUES('$user_id','$resume_id','$name','$sex','$date_birth','$live_in_now','$live_in','$cellphone','$email','$content')";
+        $list = mysql_query($view) or die("Update error!");
+    }
+}
+
+function resume_show()
+{
+    $args = func_get_args();
+    $key = $args[2];
+    include_once('login.php');
+    $id = get_current_user_id();
+    connect_db();
+    $view = "SELECT * FROM resumes WHERE resume_id = '$key'"
+    $list = mysql_query($view);
+    $row = mysql_fetch_array($list);
+    if($row)
+    {
+        $_id = $row['user_id'];
+        $view = "SELECT * FROM tweets AS t, (SELECT * FROM applications WHERE deleted = 0 AND resume_id = '$key') AS ap, (SELECT * from accountbindings WHERE user_id = '$id') AS ac WHERE t.deleted = 0 AND ap.tweet_id = t.tweet_id AND t.user_site_id = ac.user_site_id AND t.site_id = ac.site_id";
+        $list = mysql_query($view);
+        $row = mysql_fetch_array($list);
+        if($row or $id == $_id)
+        {
+            if($row)
+            {
+                $view = "UPDATE applications SET view_time = '".date('Y-m-d H:i:s')."' WHERE resume_id = '$key' AND tweet_id = '".$row['tweet_id']."' AND user_id = '$_id'";
+                $list = mysql_query($view);
+            }
+            $view = "SELECT * FROM resumes WHERE resume_id = '$key'"
+            $list = mysql_query($view);
+            $row = mysql_fetch_array($list);
+            theme('resume', $row);
+        }
+        else
+            die("Not authorized");
+    }
+    else
+        die("Resume not found");
+}
+
+function deal_resume($query)
+{
+    $key = (string) $query[1];
+    if(!$key)
+        die("Invalid Argument!");
+    $function = 'resume_'.$key;
+    if (!function_exists($function))
+        die("Invalid Argument!");
+    return call_user_func_array($function, $query);
+}
+
+?>
