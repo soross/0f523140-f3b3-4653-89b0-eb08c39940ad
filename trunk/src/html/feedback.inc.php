58176bd81780d23c906c99ac1b5b86016053ee3f
felixcat.gng@gmail.com <felixcat.gng@gmail.com@71d3263d-18f7-1632-4e8d-853502444960>
2011-04-05 19:02:51 +0000

    feedback/show

--- trunk/src/html/feedback.inc.php
+++ trunk/src/html/feedback.inc.php
@@ -16,9 +16,76 @@ function feedback_post($query)
     $v4uuid = str_replace("-", "", UUID::v4());
     connect_db();
     $add = "INSERT INTO feedback (
-					 feedback_id, question, description, email$id1)
-				     VALUES ('$v4uuid', '$question', '$description', '$email'$id2)";
+					 feedback_id, question, description, post_datetime, email$id1)
+				     VALUES ('$v4uuid', '$question', '$description', '".date('Y-m-d H:i:s')."', '$email'$id2)";
     $added = mysql_query($add) or die("Could not add entry!");
     echo "0";
+}
+
+function get_feedbacks($num, $page)
+{
+    include_once('login.inc.php');
+    if(!$page)
+        $page = "0";
+    if($page == "count")
+    {
+        $limit = "";
+        $select = "COUNT(*)";
+    }
+    else
+    {
+        $select = "*";
+        $page = intval($page) * $num;
+        $limit = " LIMIT $page , $num";
+    }
+    connect_db();
+    $view = "SELECT * from feedback, (SELECT nickname FROM userinfo) as ui WHERE ui.user_id = feedback.user_id OR feedback.user_id='NULL' ORDER BY post_datetime DESC$limit";
+    $list = mysql_query($view);
+    $result = array();
+    $i = 0;
+    while($row = mysql_fetch_array($list))
+        $result[$i++] = $row;
+    return $result;
+}
+
+function feedback_show($query)
+{
+    $key = (string) $query[1];
+    $content = '';
+    $results = get_received_tweets(10, $key);
+    if($key == "count")
+    {
+        echo $results[0][0];
+        return;
+    }
+    foreach($results as $r)
+    {
+        if($r['user_id'])
+            $nick = r['nickname'];
+        else
+            $nick = "游客";
+        $content .= '<div class="item">
+                        <div class="item-delete">
+                            <a class="right"></a>
+                        </div>
+                        <div class="left item-pic">
+                            <img alt="" width="50" height="50">
+                        </div>
+                        <div class="left item-content">
+                            <div class="item-blog">
+                                <a class="item-blog-name">'.$nick.'</a><a class="item-blog-title">'.r['question'].'</a>
+                            </div>
+                            <div class="item-blog-content close">
+                                '.r['description'].'
+                            </div>
+                            <div class="item-other">
+                                <a class="left item-position">'.r['email'].'</a> <a class="right item-control last delete">删除</a>
+                            </div>
+                        </div>
+                        <div class="clear">
+                        </div>
+                    </div>';
+    }
+    echo $content;
 }
 ?>
