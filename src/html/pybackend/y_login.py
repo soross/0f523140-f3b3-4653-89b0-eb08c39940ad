c41597c3685d72b411d592f3abb74caf22cef2d4
felixcat.gng <felixcat.gng@gmail.com>
2011-04-15 20:46:35 +0000

    2.0 login

--- src/html/pybackend/y_login.py
+++ src/html/pybackend/y_login.py
@@ -1,6 +1,7 @@
 from bottle import route, run, debug, template, request, validate, error, response, redirect
 from y_common import *
 from weibopy import OAuthHandler
+from urllib import quote_plus
 
 @route('/login')
 def login():
@@ -9,5 +10,5 @@ def login():
 @route('/sina/login')
 def sina_login():
     auth = OAuthHandler(consumer_key, consumer_secret)
-    auth_url = auth.get_authorization_url()
+    auth_url = auth.get_authorization_url() + "&oauth_callback=" + quote_plus(baseurl + "/sina/callback")
     redirect(auth_url)
