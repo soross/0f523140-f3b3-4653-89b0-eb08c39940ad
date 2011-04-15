384d0f1ba4c2f552833233a8377b39473c4bfa4f
felixcat.gng@gmail.com <felixcat.gng@gmail.com@71d3263d-18f7-1632-4e8d-853502444960>
2011-04-15 17:15:00 +0000

    html fix

---
+++ trunk/src/html/pybackend/y_admin.py
@@ -0,0 +1,13 @@
+from bottle import route, run, debug, template, request, validate, error, response, redirect
+
+@route('/admin/')
+def admin():
+    return template("home")
+
+@route('/admin/tag')
+def admin_tag():
+    return template("home")
+
+@post('/admin/tag/edit')
+def admin_tag():
+    return template("home")
