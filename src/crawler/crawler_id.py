66a92ba249cc80a353537d0fcf68377ecdd030d3
felixcat.gng <felixcat.gng@gmail.com>
2011-04-17 15:55:35 +0000

    preparing initial fetch

--- src/crawler/crawler_id.py
+++ src/crawler/crawler_id.py
@@ -2,22 +2,22 @@
 #coding=utf-8
 
 catelist = [
-        (0, u"传统网络Internet"),
-        (1, u"移动互联"),
-        (2, u"网游"),
-        (3, u"电子商务_B2C/团购"),
-        (4, u"软件、电信"),
-        (5, u"新媒体"),
-        (6, u"风投/投行"),
-        (7, u"其他外企"),
+        (1, u"传统网络Internet"),
+        (2, u"移动互联"),
+        (3, u"网游"),
+        (4, u"电子商务_B2C/团购"),
+        (5, u"软件、电信"),
+        (6, u"新媒体"),
+        (7, u"风投/投行"),
+        (8, u"其他外企"),
     ]
 idlist = [
-        [0, (u"超超Sandy", "d11c25990634d0e486235f1b42a55f9f", "89859ba49065135017b894df5e5a9089")],
-        [1, (u"冬冬Billy", "f6449dd703d66cf2be5274f321416958", "31c47db4cd79e43a9196871a554d0847")],
-        [2, (u"田田July", "032ec596fa363aa9bd3e5e5917f6aea4", "9e0c243fa3ff3515765510ba4010c411")],
-        [3, (u"田田Lily", "3ef7fc951a66918dd1cd722485fe1686", "74f524fe376ce995703e73e63407684f")],
-        [4, (u"小朱Linda", "0c10534f393fe08451edb140e3b1150d", "423fd333a2542dbf6e2a38119a6e7e04")],
-        [5, (u"小王trueman", "1f8f7db82cdbc346a91840cef2bc1cb9", "a16ead9ee3b6b3f43e601de127275ddc")],
-        [6, (u"小毕Simon", "4151efe34301f5fddb9d34fc72e5f4a4", "dc4a07e8b7936d688726604a7442e4bc")],
-        [7, (u"小黄Lily", "8ee871ac7d18e0141b54077fefd58af6", "dc5cda5a9a4ab5c5a5cd53f85f1f7915")],
+        [1, (u"超超Sandy", "d11c25990634d0e486235f1b42a55f9f", "89859ba49065135017b894df5e5a9089")],
+        [2, (u"冬冬Billy", "f6449dd703d66cf2be5274f321416958", "31c47db4cd79e43a9196871a554d0847")],
+        [3, (u"田田July", "032ec596fa363aa9bd3e5e5917f6aea4", "9e0c243fa3ff3515765510ba4010c411")],
+        [4, (u"田田Lily", "3ef7fc951a66918dd1cd722485fe1686", "74f524fe376ce995703e73e63407684f")],
+        [5, (u"小朱Linda", "0c10534f393fe08451edb140e3b1150d", "423fd333a2542dbf6e2a38119a6e7e04")],
+        [6, (u"小王trueman", "1f8f7db82cdbc346a91840cef2bc1cb9", "a16ead9ee3b6b3f43e601de127275ddc")],
+        [7, (u"小毕Simon", "4151efe34301f5fddb9d34fc72e5f4a4", "dc4a07e8b7936d688726604a7442e4bc")],
+        [8, (u"小黄Lily", "8ee871ac7d18e0141b54077fefd58af6", "dc5cda5a9a4ab5c5a5cd53f85f1f7915")],
 	]
