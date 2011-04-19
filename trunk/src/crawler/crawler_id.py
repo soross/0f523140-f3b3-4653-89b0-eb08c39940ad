b512b738081c22e527885cc5892a60bcd18863c0
felixcat.gng@gmail.com <felixcat.gng@gmail.com@71d3263d-18f7-1632-4e8d-853502444960>
2011-04-19 13:49:52 +0000

    add new crawler

--- trunk/src/crawler/crawler_id.py
+++ trunk/src/crawler/crawler_id.py
@@ -18,7 +18,9 @@ idlist = [
         [2, (u"新潮-独", "e17cec5e79cd81704aa4b73d15caf639", "983582e2b4b880cc35caa7ac38ce3449")],
         [2, (u"移动互联2", "6193a010668f5dcc106e671babf4fbe1", "605e4449590b2c3d96d34b049f96fdbd")],
         [3, (u"田田July", "032ec596fa363aa9bd3e5e5917f6aea4", "9e0c243fa3ff3515765510ba4010c411")],
+        [3, (u"网游2", "743700d7ec385d0b740ecc9a4ef519d4", "b89c6d406e235ab3c49ccea1d82b5622")],
         [4, (u"田田Lily", "3ef7fc951a66918dd1cd722485fe1686", "74f524fe376ce995703e73e63407684f")],
+        [4, (u"电子商务临2", "669001d16b1641a2138d529b435eaefb", "13f58a73995fd2b80bc0aa875ad363f0")],
         [5, (u"小朱Linda", "0c10534f393fe08451edb140e3b1150d", "423fd333a2542dbf6e2a38119a6e7e04")],
         [5, (u"软件电信2", "f577ad7699dc1a74330bc41329c2ce71", "20cc173dbb46a6784588767e71de03ef")],
         [6, (u"小王trueman", "1f8f7db82cdbc346a91840cef2bc1cb9", "a16ead9ee3b6b3f43e601de127275ddc")],
@@ -26,4 +28,5 @@ idlist = [
         [7, (u"小毕Simon", "4151efe34301f5fddb9d34fc72e5f4a4", "dc4a07e8b7936d688726604a7442e4bc")],
         [7, (u"风投2", "b14cbaaae5e46079770ea77feeaa0c91", "2dd7a1e5bb6036d69b2c983aa3f2a682")],
         [8, (u"小黄Lily", "8ee871ac7d18e0141b54077fefd58af6", "dc5cda5a9a4ab5c5a5cd53f85f1f7915")],
+        [8, (u"外企名企2", "6712203ce54da88930f72014a0ef90bd", "d97779c02d60cacbabd0c398cb8acd93")],
 	]
