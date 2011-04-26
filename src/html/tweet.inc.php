19a6a47f2721b686eeed88de227c5989d9176bc6
felixcat.gng <felixcat.gng@gmail.com>
2011-04-26 12:57:26 +0000

    added pic upload

--- src/html/tweet.inc.php
+++ src/html/tweet.inc.php
@@ -17,11 +17,15 @@ function tweet_post()
         die("Invalid argument!");
     include_once('sinaoauth.inc.php');
     $c = new WeiboClient(SINA_AKEY, SINA_SKEY, $GLOBALS['user']['sinakey']['oauth_token'], $GLOBALS['user']['sinakey']['oauth_token_secret']);
-    $msg = $c -> update($content);
+    if($_FILES['upload'])
+        $msg = $c -> upload($content, '@'.$_FILES['upload']);
+    else
+        $msg = $c -> update($content);
     if ($msg === false || $msg === null){
         echo "Error occured";
         return false;
     }
+    
     if (isset($msg['error_code']) && isset($msg['error'])){
         echo ('Error_code: '.$msg['error_code'].';  Error: '.$msg['error'] );
         return false;
@@ -35,6 +39,14 @@ function tweet_post()
                      post_screenname, profile_image_url, source)
                      VALUES (1, '$v4uuid', '".$msg['user']['id']."', '$content', '".date("Y-m-d H:i:s" ,strtotime($msg["created_at"]))."',
                      $cate, '".$msg['id']."', '".$msg["user"]["name"]."', '".$msg["user"]["profile_image_url"]."', '".$msg["source"]."')";
+    if($msg['thumbnail_pic'])
+        $add = "INSERT INTO pending_tweets (
+                     site_id, tweet_id, user_site_id, content, post_datetime,
+                     type, tweet_site_id,
+                     post_screenname, profile_image_url, source, thumbnail)
+                     VALUES (1, '$v4uuid', '".$msg['user']['id']."', '$content', '".date("Y-m-d H:i:s" ,strtotime($msg["created_at"]))."',
+                     $cate, '".$msg['id']."', '".$msg["user"]["name"]."', '".$msg["user"]["profile_image_url"]."', '".$msg["source"]."', '".$msg['thumbnail_pic']."')";
+    
     $added = mysql_query($add) or die("Could not add entry!");
     echo "0";
 }
